8e1a8b42577dc69062094aa5cc8e5b0c161a6191
KONE RAMLALSURESH <104709090+Sur-3071@users.noreply.github.com>
2024-01-05 21:27:56 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -14,9 +14,18 @@
                     input.value=s
                 }                      
  }
+ i=0
+ var s1="Calculator"
+ function strappend()
+ {
+    document.getElementById("cal1").innerHTML+=s1.charAt(i);
+    i++;
+    setTimeout(strappend,100);
+ }
  function backslash()
  {
     var input = document.getElementById("fname");
     var newstring = input.value.slice(0,input.value.length-1);
     input.value = newstring;
  }
+ strappend();
